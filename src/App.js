002d1512ed82002c807c4af9a99faae5b8ec8b8d
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-06-13 16:08:14 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
+import NavBar from './components/Navbar/Navbar';
+import { Home } from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      setup 
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+    </>
   );
 }
 
